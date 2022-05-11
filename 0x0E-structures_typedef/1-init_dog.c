00713f53fd45577e53c748cdf68f4f1005cc5302
ssozi91 <raymossekamdi@gmail.com>
2022-05-10 21:25:00 -0700

    made and added the preprocessor and structure work

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,19 @@
+#include "dog.h"
+
+/**
+ * init_dog - Program that initializes certain variables
+ * @d: Struct for initializaiton
+ * @name: Holds name char array
+ * @age: Holds age float value
+ * @owner: Holds owner name char array
+ * Return: Nothing
+ */
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	if (d)
+	{
+		d->name = name;
+		d->age = age;
+		d->owner = owner;
+	}
+}
